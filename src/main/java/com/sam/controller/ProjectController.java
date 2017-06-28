e8c598622e8e83e56d6f7f9c9c2081bd8019c097
sandeep <sandeepgoud0612@gmail.com>
2017-06-27 23:50:51 -0400

    Project Controller.

--- src/main/java/com/sam/controller/ProjectController.java
+++ src/main/java/com/sam/controller/ProjectController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,28 @@ public class ProjectController {
 	public ResponseEntity<List<Project>> getAllProjects() {
 		List<Project> projects = projectService.getAllProjects();
 		return new ResponseEntity<List<Project>>(projects, HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Project> createProject(@RequestBody Project project) {
+		Project newProject = projectService.createProject(project);
+		return new ResponseEntity<Project>(newProject, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Project> updateProject(@PathVariable String id, @RequestBody Project project) {
+		Project projectDB = projectService.getProjectById(id);
+		if (!StringUtils.isEmpty(projectDB)) {
+			Project projectUpdated = projectService.updateProject(projectDB);
+			return new ResponseEntity<Project>(projectUpdated, HttpStatus.OK);
+		}
+		return new ResponseEntity<Project>(HttpStatus.NOT_MODIFIED);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteProject(String id) {
+		projectService.deleteProject(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
 }
