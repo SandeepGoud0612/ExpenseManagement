d9489d0bc5c607020930e7d9dc086a8d693aa427
sandeep <sandeepgoud0612@gmail.com>
2017-06-28 00:04:33 -0400

    Payment Controller Update.

--- src/main/java/com/sam/controller/PaymentController.java
+++ src/main/java/com/sam/controller/PaymentController.java
@@ -3,17 +3,8 @@
  */
 package com.sam.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.sam.model.Payment;
-import com.sam.service.PaymentService;
 
 /**
  * @author Sandeep
@@ -21,14 +12,5 @@ import com.sam.service.PaymentService;
 @RestController
 @RequestMapping(value = "/payment")
 public class PaymentController {
-
-	@Autowired
-	private PaymentService paymentService;
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Payment> getPaymentById(@PathVariable String id) {
-		Payment payment = paymentService.getPaymentById(id);
-		return new ResponseEntity<Payment>(payment, HttpStatus.OK);
-	}
 
 }
